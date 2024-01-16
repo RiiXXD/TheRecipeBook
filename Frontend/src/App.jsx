2c7a1a125d98ab0c0a6f54559074d847cc541420
VaibhavBhardwaj23 <majesty.v2507@gmail.com>
2024-01-16 18:12:17 +0530

    Homepage and Page Not Found are Done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,23 @@
 
 import './App.css'
+import HomePage from './pages/HomePage'
+import PageNotFound from "./pages/PageNotFound"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
 
 
   return (
-    <>
-     <p>Hello World</p>
-    </>
+    <BrowserRouter>
+ 
+   <Routes>
+
+    <Route index path='/' element={<HomePage/>}/>
+    <Route path='*' element={<PageNotFound/>}/>
+   </Routes>
+    
+    </BrowserRouter>
+  
   )
 }
 
